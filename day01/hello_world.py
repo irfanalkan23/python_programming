9240ccb6137df82be3025f0248ec354ae213091e
irfan <23pointalpha@gmail.com>
2024-07-23 17:27:31 +0300

    Initial commit

---
+++ day01/hello_world.py
@@ -0,0 +1,30 @@
+print('hello world')  # comment
+
+"""
+multi line comment
+"""
+
+"""
+Escape Sequences:
+/n	new line
+/t	horizontal tab
+//	backslash
+/”	double quote
+/’	single quote
+"""
+
+print('I love "python"')
+print("I love \"python\"")
+print('I love \'python\'')
+
+student_name='John Wick'
+age=25
+gpa=3.5
+is_married=True
+
+print(type(age))
+print(type(gpa))
+print(type(is_married))
+
+
+
